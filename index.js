3a3c12cfee9d7db3af94248a73c9d997a57fa207
Azer Sioud <56733438+AzerSD@users.noreply.github.com>
2020-05-23 11:30:47 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const year = new Date().getFullYear() ;
-const bday = new Date(year, 4, 29, 23, 59, 59).getTime();
+const year = new Date().getFullYear();
+let bday = new Date(year, 4, 5, 8, 39).getTime();
 console.log(bday);
 
 // countdown
-let timer = setInterval(function() {
+let timer = setInterval(function () {
 
   // get today's date
   const today = new Date().getTime();
@@ -21,12 +21,97 @@ let timer = setInterval(function() {
   document.getElementById("timer").innerHTML =
     "<div class=\"days\"> \
   <div class=\"numbers\">" + days + "</div>days</div> \
-<div class=\"hours\"> \
+  <div class=\"hours\"> \
   <div class=\"numbers\">" + hours + "</div>hours</div> \
-<div class=\"minutes\"> \
+  <div class=\"minutes\"> \
   <div class=\"numbers\">" + minutes + "</div>minutes</div> \
-<div class=\"seconds\"> \
+  <div class=\"seconds\"> \
   <div class=\"numbers\">" + seconds + "</div>seconds</div> \
-</div>";
+  </div>";
 
+  //after the counting ends
+  if (days + hours + minutes + seconds == 0) {
+    console.log('fuck')
+    clearInterval(timer);
+    document.getElementsByClassName('fade')[0].style.opacity = '0';
+    document.getElementsByClassName('fade')[1].style.opacity = '0';
+
+    setTimeout(function () {
+      document.getElementsByClassName('containerr')[0].innerHTML = '';
+      document.getElementsByClassName('wrapper')[0].remove();
+      document.getElementsByTagName('footer')[0].remove();
+      document.getElementsByClassName('lights')[0].style.height = '119';
+      var btn = document.createElement("BUTTON");
+      btn.innerHTML = "Turn on lights";
+      btn.classList.add('faded')
+      btn.classList.add('action-button')
+      btn.classList.add('shadow')
+      btn.classList.add('animate')
+      btn.classList.add('color')
+      document.getElementsByClassName('containerr')[0].appendChild(btn);
+
+      butt = [
+        function light() {
+          document.getElementsByClassName('lightrope')[0].style.visibility = 'visible';
+          document.getElementsByTagName('body')[0].style.background = '#153c80';
+          document.getElementsByClassName('faded')[0].style.opacity = '0';
+          setTimeout(function () {
+            btn.innerHTML = "play music";
+            document.getElementsByClassName('faded')[0].style.opacity = '1';
+          }, 800)
+        },
+
+        function music() {
+          document.getElementsByClassName('song')[0].play();
+          document.getElementsByClassName('faded')[0].style.opacity = '0';
+          setTimeout(function () {
+            btn.innerHTML = "let's decorate";
+            document.getElementsByClassName('faded')[0].style.opacity = '1';
+          }, 800)
+        },
+
+        function decorate() {
+          document.getElementsByClassName('faded')[0].style.opacity = '0';
+          setTimeout(function () {
+            document.getElementsByClassName('banner')[0].style.visibility = 'visible';
+            document.getElementsByClassName('fade')[0].style.opacity = '1';
+            document.getElementsByClassName('faded')[0].style.opacity = '1';
+            btn.innerHTML = "most delicious cake ever";
+          }, 800)
+        },
+
+        function cake() {
+          setTimeout(function () {
+            btn.innerHTML = "happy birthday";
+            document.getElementsByClassName('faded')[0].style.opacity = '1';
+          }, 800)
+        },
+
+        function msg() {
+          setTimeout(function () {
+            btn.innerHTML = "let's decorate";
+            document.getElementsByClassName('faded')[0].style.opacity = '1';
+          }, 800)
+        }
+      ]
+
+      i = 0;
+      document.getElementsByTagName('button')[0].addEventListener("click", function () {
+        butt[i]();
+        console.log(i)
+        i++
+      })
+    }, 1000);
+
+  }
 }, 1000);
+
+
+
+
+function setime() {
+  today = new Date().getTime();
+  bday = today + 2000
+
+}
+// setime();
